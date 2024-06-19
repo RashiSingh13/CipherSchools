64242372ce283708e424d6dc3759c3e9063ca628
RashiSingh13 <rashisingh23@lpu.in>
2024-06-20 01:32:21 +0530

    initial commit

---
+++ Lecture20/to-do-list/src/screens/ToDoScreen.js
@@ -0,0 +1,65 @@
+// import { Component } from "react";
+
+import {useState} from "react";
+import Task from "../Components/task";
+
+// class ToDoScreen extends Component {
+
+//     state = {
+//         taskCount: 0,
+//     }
+
+//     render () {
+//         return  (
+//             <div className="screen">
+//         <h1 className="ui heading center">To Do List </h1>
+//         <div>
+//             <button 
+//                 onClick={ ( e ) =>{
+//                 this.setState({ 
+//                     ...this.state, 
+//                     taskCount: this.state.taskCount +1,
+//                 });
+               
+//                 console.log( this.state.taskCount);
+//             }
+//             } className="ui secondary button">Add Task</button>
+//             <p>
+//                 The number of tasks are: {this.state.taskCount }</p>
+
+    
+//         </div>
+//         </div>
+//         );
+//     }
+// }
+
+
+//functional component
+const ToDoScreen = () =>{
+
+    const [ taskList, setTaskList] = useState( [ ] );
+
+
+    return  (
+                    <div className="screen">
+                <h1 className="ui heading center">To Do List </h1>
+                <div>
+                    <button 
+                        onClick={ ( e ) =>{
+                       setTaskList ( [ ...taskList,
+                        {
+                            title: "Go to Gym",
+                            description: "I will go to gym at 7 in the morning.",
+                        },
+                       ]);
+                    }
+                    } className="ui secondary button">Add Task</button>
+                    { taskList.map ( (task) =>(
+                <Task />
+                    ))}      
+                </div>
+                </div>
+                );
+};
+export default ToDoScreen;
